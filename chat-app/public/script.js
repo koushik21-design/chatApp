cfcd1f025731c41f21cf40c6bf289e37a02b4def
Yeruva Koushik Reddy <130329397+koushik21-design@users.noreply.github.com>
2025-07-16 09:43:56 +0530

    Update script.js

--- chat-app/public/script.js
+++ chat-app/public/script.js
@@ -11,24 +11,23 @@ async function sendMessage() {
 
   try {
     const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
-  method: "POST",
-  headers: {
-    "Authorization": "Bearer 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",
-    "HTTP-Referer": "https://www.chatBotAi.com", // Optional
-    "X-Title": "chatBotAi", // Optional
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    "model": "google/gemma-3-12b-it:free",
-    "messages": [
-      {
-        "role": "user",
-        "content": input
-      }
-    ]
-  })
-});
-      
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer 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",
+        "HTTP-Referer": "https://www.chatBotAi.com", // Optional
+        "X-Title": "chatBotAi", // Optional
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "model": "google/gemma-3-12b-it:free",
+        "messages": [
+          {
+            "role": "user",
+            "content": input
+          }
+        ]
+      })
+    });
 
     const data = await response.json();
     const markdownText = data.choices?.[0]?.message?.content || 'No response received.';
